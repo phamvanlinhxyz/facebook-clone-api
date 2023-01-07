662e777b05cf3a56f86263fc783461b7548c6130
phamvanlinhxyz <linh.pv194094@sis.hust.edu.vn>
2023-01-07 22:58:54 +0700

    Chức năng like

--- routes/PostLike.js
+++ routes/PostLike.js
@@ -4,6 +4,9 @@ const express = require('express');
 const postLikeRoutes = express.Router();
 const auth = require('../middlewares/auth');
 
+// Like/Unlike bài viết
 postLikeRoutes.post('/:postId', auth, asyncWrapper(postLikeController.action));
+// Lấy danh sách like bài viết
+postLikeRoutes.get('/:postId', auth, asyncWrapper(postLikeController.list));
 
 module.exports = postLikeRoutes;
